c7872e7bbd1c45309930daaead1921f4bf24ecb4
Shin-sangwon <dnjsml10@gmail.com>
2022-06-27 17:16:59 +0900

    add testpage

---
+++ src/pages/test.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+// markup
+const TestPage = () => {
+  return (
+    <main>
+      <title>Test Page</title>
+      <h1>Test Page</h1>
+
+      <div>my first blog</div>
+    </main>
+  );
+};
+
+export default TestPage;
