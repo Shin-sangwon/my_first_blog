6cd533bb910ea544f828c960bea9ba1a7a3d4296
Shin-sangwon <dnjsml10@gmail.com>
2022-06-28 10:21:08 +0900

    add test-space

---
+++ src/pages/test-space.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+// markup
+const TestSpacePage = () => {
+  return (
+    <main>
+      <title>Test Space Page</title>
+      <h1>Test Space Page</h1>
+
+      <div>my first blog</div>
+    </main>
+  );
+};
+
+export default TestPage;
